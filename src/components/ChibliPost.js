e05d8e018724aeb8df6715a1b8c76ac9eb310156
Ploylada Pharikarn <parames.m4xexp@gmail.com>
2021-01-16 21:20:04 +0700

    Add close x button on post photo

--- src/components/ChibliPost.js
+++ src/components/ChibliPost.js
@@ -7,8 +7,9 @@ function ChibliPost(props) {
     return (
         <div className="chibli-post fixed top-0 bottom-0 left-0 right-0 z-999 flex justify-center items-center ">
             <div className="chibli-post-bg bg-black bg-opacity-75 top-0 absolute bottom-0 left-0 right-0" onClick={onChibliClose}/>
-            <div className="chibli-post-content relative bg-white max-w-screen-md rounded p-4">
-                <img src={ chibli.thumbnailUrl } alt="" className="block max-w-xl ml-auto mr-auto" />
+            <div className="chibli-post-content relative bg-white lg:max-w-screen-md md:max-w-lg sm:max-w-lg rounded p-4">
+                <svg className="w-6 h-6 absolute right-0 top-1 transform hover:rotate-45 transition-transform cursor-pointer" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" onClick={onChibliClose} /></svg>
+                <img src={ chibli.thumbnailUrl } alt="" className="block lg:max-w-xl md:max-w-md ml-auto mr-auto mt-4" />
                 <h4 className="text-black text-center font-semibold m-6"> { chibli.title } </h4>
             </div>
         </div>
